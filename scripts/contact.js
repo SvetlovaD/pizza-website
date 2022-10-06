2f3baaade24878220f4bec0248cd6641492ad692
Daria Shut <d.svetlova29@gmail.com>
2022-10-06 12:27:16 +0300

    refactored

--- scripts/contact.js
+++ scripts/contact.js
@@ -58,40 +58,4 @@ button.addEventListener("click", function(e) {
     } else {alert("Your message isn't sent! Check the accuracy of your data!")}
 });
 
-let btn1 = document.querySelectorAll(".btn1");
-let btn2 = document.querySelectorAll(".btn2");
-let btn3 = document.querySelectorAll(".btn3");
-
-let page1 = document.querySelector("#place1");
-let page2 = document.querySelector("#place2");
-let page3 = document.querySelector("#place3");
-
-console.log(page1);
-
-
-btn1.forEach(btn => {
-    btn.addEventListener("click", function() {
-        page1.style.opacity = "1";
-        page2.style.opacity = "0";
-        page3.style.opacity = "0";
-        
-    })
-});
-
-btn2.forEach(btn => {
-    btn.addEventListener("click", function() {
-        page1.style.opacity = "0";
-        page2.style.opacity = "1";
-        page3.style.opacity = "0";
-    })
-});
-
-btn3.forEach(btn => {
-    btn.addEventListener("click", function() {
-        page1.style.opacity = "0";
-        page2.style.opacity = "0";
-        page3.style.opacity = "1";
-    })
-});
-
 
