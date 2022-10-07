2378ecd332809219354bb725ef2668c8d40fac67
Daria Shut <d.svetlova29@gmail.com>
2022-10-07 03:31:40 +0300

    updated

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,22 +1,22 @@
-let images = document.querySelectorAll(".blog-img");
+let images = document.querySelectorAll(".menu-img");
 
 
 images.forEach(img => {
     img.addEventListener("click", function() {
-        if (img.style.transform == "scale(1.3)") {
+        if (img.style.transform == "scale(3.5)") {
             img.style.transform = "none";
             img.style.zIndex = "1";
             
         }
         else {
-            img.style.transform = "scale(1.3)";
+            img.style.transform = "scale(3.5)";
             img.style.zIndex = "3";
             
         }
     })
 
     img.addEventListener("mouseover", function() {
-        img.style.transform = "scale(1.3)";
+        img.style.transform = "scale(3.5)";
         img.style.zIndex = "3"; 
         
     })
