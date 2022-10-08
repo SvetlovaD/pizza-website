9506d1cf450f2db011cd89859f77dd5e6f3bfcf0
Daria Shut <d.svetlova29@gmail.com>
2022-10-09 02:51:44 +0300

    media blog

--- scripts/blog.js
+++ scripts/blog.js
@@ -3,20 +3,20 @@ let images = document.querySelectorAll(".blog-img");
 
 images.forEach(img => {
     img.addEventListener("click", function() {
-        if (img.style.transform == "scale(1.3)") {
+        if (img.style.transform == "scale(1.2)") {
             img.style.transform = "none";
             img.style.zIndex = "1";
             
         }
         else {
-            img.style.transform = "scale(1.3)";
+            img.style.transform = "scale(1.2)";
             img.style.zIndex = "3";
             
         }
     })
 
     img.addEventListener("mouseover", function() {
-        img.style.transform = "scale(1.3)";
+        img.style.transform = "scale(1.2)";
         img.style.zIndex = "3"; 
         
     })
