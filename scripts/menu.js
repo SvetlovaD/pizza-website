28f69df7d408482200f0b2f72db05edf8d1345bd
Daria Shut <d.svetlova29@gmail.com>
2022-10-09 04:50:15 +0300

    media menu

--- scripts/menu.js
+++ scripts/menu.js
@@ -3,20 +3,20 @@ let images = document.querySelectorAll(".menu-img");
 
 images.forEach(img => {
     img.addEventListener("click", function() {
-        if (img.style.transform == "scale(3.5)") {
+        if (img.style.transform == "scale(2.4) translateX(20px)") {
             img.style.transform = "none";
             img.style.zIndex = "1";
             
         }
         else {
-            img.style.transform = "scale(3.5)";
+            img.style.transform = "scale(2.4) translateX(20px)";
             img.style.zIndex = "3";
             
         }
     })
 
     img.addEventListener("mouseover", function() {
-        img.style.transform = "scale(3.5)";
+        img.style.transform = "scale(2.4) translateX(20px)";
         img.style.zIndex = "3"; 
         
     })
